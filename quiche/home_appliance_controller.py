ab20b59dd72b0c32ab8edee613caf6ed8f2b8794
EndoHizumi <endohizumi0114@gmail.com>
2020-08-21 02:36:42 +0900

    wip

--- quiche/home_appliance_controller.py
+++ quiche/home_appliance_controller.py
@@ -20,24 +20,43 @@ def send(command, name):
         else:
             raise UnknownTypeError('')
 
+
 def show(name):
     command_file_path = os.path.join(command_files_dir, f'{name}.json')
     with open(command_file_path) as command_json:
         commands = json.load(command_json)
         action_lists = {}
         action_lists['name'] = name
-        action_lists['commands'] = list(commands.keys())
+        action_lists['commands'] = list(commands.keys())[1:]
         action_lists['type'] = commands['type']
     return json.dumps(action_lists)
 
-def read(name, button_no, action):
+
+def read(name: str, button_no: list, action: list):
     """adrsirで記録した赤外線パターンをJSONに保存する
 
     記録したボタン番号と記憶したリモコンのボタンの名前（テレビなら、音量アップや８チャンネルなど）、操作される機器の名前を指定する
 
     Args:
         name : 操作される機器の名前（テレビなど。日本語可）
-        button_no : ADRSIRの記録したボタンの番号
+        button_no : ADRSIRの記録したボタンの番号。
         action : 記憶させた赤外線パターンの登録名（テレビの電源を入れるなら、wakeなど。日本語可）
-    """    
+    """
 
+    pattern_list = {}
+    result = []
+    command_file_path = os.path.join(command_files_dir, f'{name}.json')
+    if (os.path.exists(command_file_path)):
+        with open(command_file_path) as f:
+            pattern_list = json.load(f)
+    else:
+        pattern_list['type'] = 'ir'
+
+    for button, action_name in zip(button_no, action):
+        pattern_list[action_name] = ir.read(button)
+        result += [f'registered {action_name} of {name}']
+
+    with open(command_file_path, 'w') as command_json:
+        json.dump(pattern_list, command_json)
+
+    return result
