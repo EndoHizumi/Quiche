ebdde6bbdee059da306121fbfad698e4e20fec88
Endo_Hizumi <endohizumi0114@gmail.com>
2020-08-25 12:57:12 +0900

    add type hinting and Docstring

--- quiche/home_appliance_controller.py
+++ quiche/home_appliance_controller.py
@@ -3,11 +3,22 @@ from quiche.exceptions.exceptions import UnknownTypeError
 import requests
 import os
 import json
+from typing import Dict, List
 
 command_files_dir = os.path.join(os.path.dirname(__file__), 'commands')
 
 
-def send(command, name):
+def send(command: str, name: str) -> str:
+    """入力された登録名とコマンド名に紐づいた赤外線パターンを送信する
+
+    Args:
+        command: 登録した赤外線パターンの名前
+        name: 登録した家電の名前
+
+    Raises:
+        UnknownTypeError: 指定された家電のtypeの値がirかwi-fiのどちらでもない場合に発生する
+    """
+
     command_file_path = os.path.join(command_files_dir, f'{name}.json')
     with open(command_file_path) as command_json:
         commands = json.load(command_json)
@@ -21,7 +32,13 @@ def send(command, name):
             raise UnknownTypeError('')
 
 
-def show(name):
+def show(name: str) -> Dict[str, str]:
+    """登録されたコマンド名一覧を返す
+
+    Args:
+        name: 登録した家電の名前
+    """
+
     command_file_path = os.path.join(command_files_dir, f'{name}.json')
     with open(command_file_path) as command_json:
         commands = json.load(command_json)
@@ -32,7 +49,7 @@ def show(name):
     return json.dumps(action_lists)
 
 
-def read(name: str, button_no: list, action: list):
+def read(name: str, button_no: list, command: list) -> List[str]:
     """adrsirで記録した赤外線パターンをJSONに保存する
 
     記録したボタン番号と記憶したリモコンのボタンの名前（テレビなら、音量アップや８チャンネルなど）、操作される機器の名前を指定する
@@ -40,7 +57,7 @@ def read(name: str, button_no: list, action: list):
     Args:
         name : 操作される機器の名前（テレビなど。日本語可）
         button_no : ADRSIRの記録したボタンの番号。
-        action : 記憶させた赤外線パターンの登録名（テレビの電源を入れるなら、wakeなど。日本語可）
+        command : 記憶させた赤外線パターンの登録名（テレビの電源を入れるなら、wakeなど。日本語可）
     """
 
     pattern_list = {}
